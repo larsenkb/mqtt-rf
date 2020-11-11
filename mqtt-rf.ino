db4a2145f832deec318d2d099d9d0f742a24b392
Kent B. Larsen <kent.b.larsen@gmail.com>
2020-11-11 11:38:09 -0800

    add all 5 button; works if no Blynk server

--- mqtt-rf.ino
+++ mqtt-rf.ino
@@ -1,31 +1,41 @@
 /*
- MQTT <--> RF <--> BLYNK
+ Zap(RF) <--> MQTT <--> BLYNK
 
  Wemos D1 mini is connected to a 433MHz transmitter and a 433MHz receiver
- which are used to communicate with Zap Transmitter and Receiver pair.
+ which are used to communicate with Zap Etecity wireless system.
+     https://www.amazon.com/Etekcity-5-Channel-Wireless-Control-Receivers/dp/B00FK6SK24
  
- When the Zap controller is activated, this device as well as the Zap receiver
- are notified. This device will convert the received RF message to a MQTT msg
- and publish it and to BLYNK messages.
+ When a button on the Zap controller is depressed, this bridge as well as the Zap socket
+ receive the RF code. This bridge will:
+     1) convert the received RF code to a MQTT msg and publish it.
+     2) convert the received RF code to a BLYNK message and push it to the local Blynk server.
+     3) re-transmit the RF code one time via the 433MHz transmitter.
 
- When this device receives an MQTT msg it converts it to a 433MHz Zap message
- and transmits it to the Zap receiver.
-
- When this device receives BLYNK virtual button messages for V1 and V2, it
- converts them to Zap messages and transmits them to the 433MHz receiver.
+ When this bridge receives an MQTT message:
+     1) it converts it to a 433MHz Zap code and transmits it to the Zap socket.
+     2) it converts it to a Blynk message and pushes it to the local Blynk server.
+     3) publishes an MQTT status message.
+     
+ When this bridge receives BLYNK virtual button messages for V1 through V5:
+     1) it converts it to a 433MHz Zap code and transmits it to the Zap socket.
+     2) publishes an MQTT status message.
  
  
-
  It will reconnect to the server if the connection is lost using a blocking
  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
  achieve the same result without blocking the main loop.
 
+ This bridge will function properly without a local Blynk Server acting as a bridge
+ between the MQTT broker and the Zap controller. When and if a local Blynk Server becomes
+ active, this bridge will add it to its bridge functions.
+ 
  To install the ESP8266 board, (using Arduino 1.6.4+):
   - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
        http://arduino.esp8266.com/stable/package_esp8266com_index.json
   - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
   - Select your ESP8266 in "Tools -> Board"
 
+These are the ON/OFF codes of my Zap controller:
    ZAP    ZAP
    ON     OFF
    -----  -----
@@ -37,30 +47,46 @@
 
 */
 
-#include <ESP8266WiFi.h>
+#define BLYNK_TIMEOUT_MS  750  // must be BEFORE BlynkSimpleEsp8266.h doesn't work !!!
+#define BLYNK_HEARTBEAT   17   // must be BEFORE BlynkSimpleEsp8266.h works OK as 17s
+
+#include <BlynkSimpleEsp8266.h>
 #include <PubSubClient.h>
 #include <RCSwitch.h>
-#include <SimpleDHT.h>
-#include <BlynkSimpleEsp8266.h>
+
 
 #define RF_TX_PIN     D4
 #define RF_RX_PIN     D7
-///#define RF_TX_EN_PIN  D8
-///#define DHT11_PIN     D1
 
-//#define RF_PUB_TOPIC   "rfgw/tomqtt"
-//#define RF_SUB_TOPIC   "rfgw/torf"
-///#define PUB_DHT11_TOPIC   "rfgw/dht11"
+#define EN_BLYNK      1
 
-#define NEXT_DELAY      500
-#define NEXT_RF_DELAY   500  
-///#define NEXT_DHT_DELAY  (1000 * 60)
 
-#define EN_BLYNK    1
+typedef struct {
+  int   buttonCode;
+  char  mqttMsg[16];
+  char  mqttPld[8];
+  int   blynkVirtPin;
+  int   blynkVirtPinValue;
+} ZapButton_t;
+
+// Initalize zap array, place OFF button before ON button
+ZapButton_t zap[5*2] = {
+  {21820, "rfgw/switch1", "OFF", V1, 0},
+  {21811, "rfgw/switch1", "ON",  V1, 1},
+  {21964, "rfgw/switch2", "OFF", V2, 0},
+  {21955, "rfgw/switch2", "ON",  V2, 1},
+  {22284, "rfgw/switch3", "OFF", V3, 0},
+  {22275, "rfgw/switch3", "ON",  V3, 1},
+  {32820, "rfgw/switch4", "OFF", V4, 0},
+  {23811, "rfgw/switch4", "ON",  V4, 1},
+  {29964, "rfgw/switch5", "OFF", V5, 0},
+  {29955, "rfgw/switch5", "ON",  V5, 1}
+};
+
+BlynkTimer timer;
 
 RCSwitch rf = RCSwitch();
 
-///SimpleDHT11 dht11(DHT11_PIN);
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
@@ -73,145 +99,115 @@ const char *mqtt_server = "192.168.1.65";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-long lastMsg = 0;
-int value = 0;
-unsigned long next;
-unsigned long next_rf;  
-///unsigned long next_dht;
 
-
-
-void setup_wifi(void)
+// check every 11s if connected to Blynk server
+void CheckConnection()
 {
-
-  delay(10);
-
-  WiFi.begin(ssid, pass);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  if (!Blynk.connected()) {
+    Blynk.connect();  // try to connect to server with default timeout
   }
-
-#if EN_BLYNK
-  Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,65), 8080);
-#endif
-
-  randomSeed(micros());
 }
 
 
-#if 1
-void callback(char *topic, byte *payload, unsigned int length)
+void mqttCB(char *topic, byte *payload, unsigned int length)
 {
   char tpayload[8];
   char ttopic[16];
   unsigned long data;
+  int j;
 
+  // message should be in form of "rfgw/switchn/set"
   strncpy(tpayload, (const char *)payload, length);
   tpayload[length] = '\0';
+  // remove "/set" from message
   strncpy(ttopic, topic, 12);
   ttopic[12] = '\0';
 
-  Serial.print(ttopic); Serial.print("/set");
-  Serial.print("  ");
-  Serial.println(tpayload);
-
-  if (strcmp(topic, "rfgw/switch1/set") == 0) {
-    if (strcmp(tpayload, "ON") == 0) {
-      data = 21811;
-    } else if (strcmp(tpayload, "OFF") == 0) {
-      data = 21820;
-    } else {
-      return;
-    }
-  } else if (strcmp(topic, "rfgw/switch2/set") == 0) {
-    if (strcmp(tpayload, "ON") == 0) {
-      data = 21955;
-    } else if (strcmp(tpayload, "OFF") == 0) {
-      data = 21964;
-    } else {
-      return;
+  // search for message in Zap database and retrieve RF code, if found.
+  for (j = 0; j < 10; j += 2) {
+    if (strcmp(ttopic, zap[j].mqttMsg) == 0) {
+      if (strcmp(tpayload, zap[j].mqttPld) == 0) {
+        data = zap[j].buttonCode;
+        break;
+      } else if (strcmp(tpayload, zap[j+1].mqttPld) == 0) {
+        data = zap[j+1].buttonCode;
+        j += 1;
+        break;
+      } else {
+        return;
+      }
     }
   }
-///  if (next < millis()) {
-///    digitalWrite(RF_TX_EN_PIN, HIGH);
- ///   delay(50);    // delay to allow relay to switch
-//    topic[12] = '\0';
-    Serial.printf("%s %d\n\r", topic, data);
-    txCode(data, 186, 3);
-///    next = millis() + NEXT_DELAY;
-    client.publish(ttopic, (byte *)tpayload, strlen(tpayload), true);
-///    delay(50);    // delay to allow relay to switch
-///    digitalWrite(RF_TX_EN_PIN, LOW);
-///  }
-}
-#else
-void callback(char *topic, byte *payload, unsigned int length)
-{
-  char buff[16];
+  // if we fell out of the loop and didn't find a match just return and do nothing
+  if (j == 10) return;
 
-  if (strcmp(topic, RF_SUB_TOPIC) == 0) {
-    unsigned long data = strtoul((const char *)payload, NULL, 10);
-    if (next < millis()) {
-      digitalWrite(RF_TX_EN_PIN, HIGH);
-      delay(50);    // delay to allow relay to switch
-      Serial.printf("%s %d\n\r", topic, data);
-      txCode(data, 186, 3);
-      next = millis() + NEXT_DELAY;
-      delay(50);    // delay to allow relay to switch
-      digitalWrite(RF_TX_EN_PIN, LOW);
-    }
+  // Transmit code to the Zap socket
+  txCode(data, 186, 3);
+
+  // Push message to Blynk Server
+  if (Blynk.connected()) {
+    Blynk.virtualWrite(zap[j].blynkVirtPin, zap[j].blynkVirtPinValue);
   }
+
+  // Publish MQTT status
+  client.publish(ttopic, (byte *)tpayload, strlen(tpayload), true);
+  
+  delay(50);    // delay to allow relay to switch
 }
-#endif
+
 
 void reconnect()
 {
   // Loop until we're reconnected
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
     // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
       // Once connected, publish an announcement...
       client.publish("rfgw", "online");
       // ... and resubscribe
-///      client.subscribe("/lofi/node/12/ctr");
       client.subscribe("rfgw/switch1/set"); //RF_SUB_TOPIC);
       client.subscribe("rfgw/switch2/set"); //RF_SUB_TOPIC);
+      client.subscribe("rfgw/switch3/set"); //RF_SUB_TOPIC);
+      client.subscribe("rfgw/switch4/set"); //RF_SUB_TOPIC);
+      client.subscribe("rfgw/switch5/set"); //RF_SUB_TOPIC);
+      // would "rfgw/*/set" work???
     } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+      // Wait 1 second before retrying
+      delay(1000);
     }
   }
 }
 
-
 void setup()
 {
-//  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
-//  digitalWrite(BUILTIN_LED, LOW);
-///  pinMode(RF_TX_EN_PIN, OUTPUT);     // Initialize TX/RX relay control
-///  digitalWrite(RF_TX_EN_PIN, LOW);
   pinMode(RF_TX_PIN, OUTPUT);     // Initialize the RF_TX_PIN pin as an output
   digitalWrite(RF_TX_PIN, LOW);
 
-  Serial.begin(115200);
-  rf.enableReceive(RF_RX_PIN);  // Receiver on interrupt D7
-  setup_wifi();
+  rf.enableReceive(RF_RX_PIN);  // Receiver on pin D7
+  
+  delay(10);
+
+#if EN_BLYNK
+  Blynk.connectWiFi(ssid, pass);
+  
+  // line below needs to be BEFORE Blynk.connect()
+  timer.setInterval(11000L, CheckConnection); // check if still connected every 11s  
+  
+  Blynk.config(auth, IPAddress(192,168,1,65), 8080);
+  Blynk.connect();
+#endif
+
+  randomSeed(micros());
+
+  // Connect to MQTT broker
   client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-  next = millis() + NEXT_DELAY;
-  next_rf = millis() + NEXT_RF_DELAY;
-///  next_dht = millis() + NEXT_DHT_DELAY;
+  client.setCallback(mqttCB);
   
 }
+
 
 // This function will be called every time Overhead Button Widget
 // in Blynk app writes values to the Virtual Pin V1
@@ -219,81 +215,110 @@ BLYNK_WRITE(V1)
 {
   int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
 
+  if (pinValue != 0 && pinValue != 1)
+    return;
+    
   // process received value
-  if (pinValue == 1) {
-    txCode(21811, 186, 3);
-    client.publish("rfgw/switch1", (byte*)"ON", 2, true);
-  } else if (pinValue == 0) {
-    txCode(21820, 186, 3);
-    client.publish("rfgw/switch1", (byte*)"OFF", 3, true);
-  }
+  txCode(zap[pinValue].buttonCode, 186, 3);
+  client.publish(zap[pinValue].mqttMsg, (byte*)zap[pinValue].mqttPld, strlen(zap[pinValue].mqttPld), true);
 }
 
 // This function will be called every time Lamp Button Widget
 // in Blynk app writes values to the Virtual Pin V2
 BLYNK_WRITE(V2)
 {
-  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+  int pinValue = param.asInt(); // assigning incoming value from pin V2 to a variable
 
+  if (pinValue != 0 && pinValue != 1)
+    return;
+    
   // process received value
-  if (pinValue == 1) {
-    txCode(21955, 186, 3);
-    client.publish("rfgw/switch2", (byte*)"ON", 2, true);
-  } else if (pinValue == 0) {
-    txCode(21964, 186, 3);
-    client.publish("rfgw/switch2", (byte*)"OFF", 3, true);
-  }
+  txCode(zap[2 + pinValue].buttonCode, 186, 3);
+  client.publish(zap[2 + pinValue].mqttMsg, (byte*)zap[2 + pinValue].mqttPld, strlen(zap[2 + pinValue].mqttPld), true);
 }
+
+// This function will be called every time Virtual Button 3 
+// in Blynk app writes values to the Virtual Pin V3
+BLYNK_WRITE(V3)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V3 to a variable
+
+  if (pinValue != 0 && pinValue != 1)
+    return;
+    
+  // process received value
+  txCode(zap[4 + pinValue].buttonCode, 186, 3);
+  client.publish(zap[4 + pinValue].mqttMsg, (byte*)zap[4 + pinValue].mqttPld, strlen(zap[4 + pinValue].mqttPld), true);
+}
+
+// This function will be called every time Virtual Button 4 
+// in Blynk app writes values to the Virtual Pin V4
+BLYNK_WRITE(V4)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V4 to a variable
+
+  if (pinValue != 0 && pinValue != 1)
+    return;
+    
+  // process received value
+  txCode(zap[6 + pinValue].buttonCode, 186, 3);
+  client.publish(zap[6 + pinValue].mqttMsg, (byte*)zap[6 + pinValue].mqttPld, strlen(zap[6 + pinValue].mqttPld), true);
+}
+
+// This function will be called every time Virtual Button 5 
+// in Blynk app writes values to the Virtual Pin V5
+BLYNK_WRITE(V5)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V5 to a variable
+
+  if (pinValue != 0 && pinValue != 1)
+    return;
+    
+  // process received value
+  txCode(zap[8 + pinValue].buttonCode, 186, 3);
+  client.publish(zap[8 + pinValue].mqttMsg, (byte*)zap[8 + pinValue].mqttPld, strlen(zap[8 + pinValue].mqttPld), true);
+}
+
 
 void loop()
 {
   char buff[32];
+  int lastCode = 0;
 
-#if EN_BLYNK
-  Blynk.run();
-#endif
+  
+  if (Blynk.connected()) {
+    Blynk.run();
+  }
 
+  timer.run();
+  
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 
   if (rf.available()) {
-    Serial.printf("%d %d %d %d %d\n\r", rf.getReceivedValue(), rf.getReceivedBitlength(), rf.getReceivedDelay(), rf.getReceivedRawdata(),rf.getReceivedProtocol());
+    int rxValue = rf.getReceivedValue();
     // don't respond to every rf transmission (it is repeated several times and as long as button is pressed)
- ///   if (next_rf < millis()) {
-      //client.publish(RF_PUB_TOPIC, itoa(rf.getReceivedValue(), buff, 10));
-      next_rf = millis() + NEXT_RF_DELAY;
-      if (rf.getReceivedValue() == 21811) {
-        Blynk.virtualWrite(V1, 1);
-        client.publish("rfgw/switch1", (byte*)"ON", 2, true);
-      } else if (rf.getReceivedValue() == 21820) {
-        Blynk.virtualWrite(V1, 0);
-        client.publish("rfgw/switch1", (byte*)"OFF", 3, true);
-      } else if (rf.getReceivedValue() == 21955) {
-        Blynk.virtualWrite(V2, 1);
-        client.publish("rfgw/switch2", (byte*)"ON", 2, true);
-      } else if (rf.getReceivedValue() == 21964) {
-        Blynk.virtualWrite(V2, 0);
-        client.publish("rfgw/switch2", (byte*)"OFF", 3, true);
-      }
-///   }
+    for (int j = 0; j < 10; j++) {
+      if (rxValue == zap[j].buttonCode) {
+        if (lastCode != zap[j].buttonCode) {
+          lastCode = zap[j].buttonCode;
+          if (Blynk.connected()) {
+            Blynk.virtualWrite(zap[j].blynkVirtPin, zap[j].blynkVirtPinValue);
+          }
+          client.publish(zap[j].mqttMsg, (byte*)zap[j].mqttPld, strlen(zap[j].mqttPld), true);  // ??? what is 3rd parameter???
+          txCode(lastCode, 186, 3);
+        }
+      } 
+    }
     rf.resetAvailable();
-  }
+  } // endof: if (rx.available())...
 
-///  if (next_dht < millis()) {
-///    next_dht = millis() + NEXT_DHT_DELAY;
-///    // read without samples.
-///    byte temperature = 0;
-///    byte humidity = 0;
-///    if (dht11.read(&temperature, &humidity, NULL) == SimpleDHTErrSuccess) {
-///      sprintf(buff, "{\"hum\":\"%d\",\"temp\":\"%d\"}", (int)humidity, (int)temperature);
-///      client.publish(PUB_DHT11_TOPIC, (byte*)buff, strlen(buff), true);
-///      Serial.println(buff);
-///    }
-///  }
 }
 
+// Transmit Zap RF code by bit-bang method.
+// Assume code is 24-bit value
 void txCode(int val, int dly, int rpt)
 {
   int digit;
@@ -311,22 +336,22 @@ void txCode(int val, int dly, int rpt)
     digit = 1<<23;
     do {
       if (digit&val) {
-        digitalWrite(RF_TX_PIN, HIGH);    // set the LED off
+        digitalWrite(RF_TX_PIN, HIGH);
         delayMicroseconds(dlyh);
-        digitalWrite(RF_TX_PIN, LOW);    // set the LED off
+        digitalWrite(RF_TX_PIN, LOW);
         delayMicroseconds(dlyl);
       } else {
-        digitalWrite(RF_TX_PIN, HIGH);    // set the LED off
+        digitalWrite(RF_TX_PIN, HIGH);
         delayMicroseconds(dlyl);
-        digitalWrite(RF_TX_PIN, LOW);    // set the LED off
+        digitalWrite(RF_TX_PIN, LOW);
         delayMicroseconds(dlyh);
       }
       digit >>= 1;
     } while (digit);
     
-    digitalWrite(RF_TX_PIN, HIGH);    // set the LED off
+    digitalWrite(RF_TX_PIN, HIGH);
     delayMicroseconds(dlyl);
-    digitalWrite(RF_TX_PIN, LOW);    // set the LED off
+    digitalWrite(RF_TX_PIN, LOW);
     delayMicroseconds(dlyh);
     delay(5);
   } while (--rpt);
